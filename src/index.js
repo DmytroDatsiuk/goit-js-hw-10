f14bbb86046d1079950d5aa33869a43406b34ffe
DDimbe <109931951+DmytroDatsiuk@users.noreply.github.com>
2023-01-03 06:39:51 +0200

    fixed

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import './css/styles.css';
 import debounce from 'lodash.debounce';
 import { fetchCountries } from './fetchCountries';
 import Notiflix from 'notiflix';
+import createCountrylist from './function';
+import createCountryInfo from './createCountryInfp';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -25,46 +27,21 @@ function onIput(e) {
 
   fetchCountries(name)
     .then(country => {
-      createCountrylist(country);
-
       if (country.length > 10) {
         Notiflix.Notify.info(
           `Too many matches found. Please enter a more specific name.`
         );
-        
+        return;
       }
+      
+      list.insertAdjacentHTML('beforeend', createCountrylist(country));
 
       if (country.length === 1) {
-        createCountryInfo(country);
+        divInfo.insertAdjacentHTML('beforeend', createCountryInfo(country));
       }
     })
     .catch(error => {
       console.log(error);
       Notiflix.Notify.failure(`Oops, there is no country with that name`);
     });
-}
-
-function createCountrylist(country) {
-  const countryItem = country
-    .map(
-      ({
-        name,
-        flags,
-      }) => `<li class="country-item"><img class='country-img' src="${flags.png}" alt="" width="30" height="25"/>
-            <p>${name}</p></li>`
-    )
-    .join('');
-
-  list.insertAdjacentHTML('beforeend', countryItem);
-}
-
-function createCountryInfo(country) {
-  const countryInfo = country
-    .map(({ capital, population, languages }) => {
-      const languagesName = languages.map(({ name }) => name).join(', ');
-      return `<p>Capital: ${capital}</p>Population: ${population}<p></p><p>Languages: ${languagesName}</p>`;
-    })
-    .join('');
-
-    divInfo.insertAdjacentHTML('beforeend', countryInfo);
 }
